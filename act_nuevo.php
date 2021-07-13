8030218f37b61b26d407e026945d48b010d9c69a
AlexisCuximPoot <1828602662@conalepqroo.edu.mx>
2021-07-13 12:07:54 -0500

    Primera clase

---
+++ act_nuevo.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'db.php';
+
+try {
+    $consulta = "INSERT INTO tareas (nombre, descripcion) VALUES (:nombre, :descripcion)";
+    
+    $sql = $connect->prepare($consulta);
+
+    $sql->bindParam(':nombre', $_POST['nombre'], PDO::PARAM_STR, 25);
+    $sql->bindParam(':descripcion', $_POST['descripcion'], PDO::PARAM_STR, 100);
+    
+    $sql->execute();
+
+    $ultimoId = $connect->lastInsertId();
+    if ($ultimoId > 0) {
+        header("Location: index.php");
+    } else {
+        print_r($sql->errorInfo()); 
+    }
+} catch (\Throwable $th) {
+    echo $th->getMessage();
+    var_dump($th->getMessage());die();
+}
+
+
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App de tareas</title>
+    <style>
+        table,tr, td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <a href="nuevo.php"><button>Nuevo</button></a>
+    <table>
+        <tr>
+            <td>Id</td>
+            <td>Tarea</td>
+            <td>Acciones</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>
+                <button>Editar</button>
+                <button>Borrar</button>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
