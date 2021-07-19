bd2b97bf285df0e81b3cfd11204552d57946c8dc
AlexisCuximPoot <1828602662@conalepqroo.edu.mx>
2021-07-19 11:38:34 -0500

    la migracion de la base de datos

---
+++ migraciones/dump.sql
@@ -0,0 +1,10 @@
+-- Código de creación de la base de datos.
+CREATE DATABASE `tareas` /*!40100 COLLATE 'utf8_general_ci' */
+
+CREATE TABLE `tareas` (
+	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(150) NOT NULL,
+	`descripcion` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci';
